471c816824d8573475e4701a7d6bdf08040db4e7
RevelatorM <152555320+RevelatorM@users.noreply.github.com>
2026-01-25 16:40:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from tkinter import * #importing everything from tkinter
 
-#
+#widgets - GUI elements
+#windows - container
+
+window = Tk() #creating a window
+window.mainloop() #event listener and windowloop
